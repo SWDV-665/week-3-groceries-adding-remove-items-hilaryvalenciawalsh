92ab2f9f7f590f163642f333920e7079563f4b2e
Hilary Valencia-Walsh <hilaryvalenciawalsh@gmail.com>
2021-11-07 18:27:39 -0800

    adding read.me images

---
+++ readme.md
@@ -0,0 +1,5 @@
+![myimage-alt-tag](screenshots/pic1.png)
+![myimage-alt-tag](screenshots/pic2.png)
+![myimage-alt-tag](screenshots/pic3.png)
+![myimage-alt-tag](screenshots/pic4.png)
+![myimage-alt-tag](screenshots/pic5.png)
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,95 @@
 import { Component } from '@angular/core';
 
+import { ToastController } from '@ionic/angular';
+
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(
+    public toastController: ToastController,
+    public alertController: AlertController
+  ) {}
 
-  constructor() {}
+  title = 'Grocery List Items';
+  //list that will be read to create items from , added a few items to start the list
+  items = [
+    {
+      name: 'Sugar',
+      quantity: 4,
+    },
+    {
+      name: 'Milk',
+      quantity: 2,
+    },
+  ];
 
+  async removeItem(item, index) {
+    const toast = await this.toastController.create({
+      message: `Removing: ${item.name}`,
+      duration: 100,
+    });
+    toast.present();
+    this.items.splice(index, 1);
+  }
+
+  async addItem(data) {
+    const toast = await this.toastController.create({
+      message: `Adding: ${data.name}`,
+      duration: 100,
+    });
+    toast.present();
+    this.items.push(data);
+  }
+
+  //alert pop up
+  async addItemPopUp() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-class',
+      header: 'Enter item',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          id: 'item-name',
+          placeholder: 'Item',
+        },
+
+        //second input
+        {
+          name: 'quantity',
+          type: 'text',
+          id: 'item-id',
+          placeholder: 'Quantity',
+        },
+      ],
+
+      //button
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (data) => {
+            console.log('Cancel');
+          },
+        },
+
+        {
+          text: 'save',
+          handler: (data) => {
+            console.log('Click to add');
+            //add item to the list of items
+            this.addItem(data);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
